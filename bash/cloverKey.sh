863b3f0fcd8f4b9d0384132c84fd3b9038daa590
Anton Kuznetsov <lukarek@Antons-MacBook-Pro.local>
2021-11-05 23:57:03 +0400

    Улучшил читаемость записи после получения ключа с пода кловера

--- bash/cloverKey.sh
+++ bash/cloverKey.sh
@@ -6,7 +6,7 @@ SANDBOX=$1
 
 CLOVER=$(kubectl get pod -n "sandbox-$SANDBOX" -l "app=clover,hive=eu-fra1" -o jsonpath='{.items[].metadata.name}')
 
-key=$(kubectl -n "sandbox-$SANDBOX" exec -it $CLOVER -c "clover" -- bash -c "grep 'Verification code:' /opt/clover/logs/*.log")
+key=$(kubectl -n "sandbox-$SANDBOX" exec -it $CLOVER -c "clover" -- bash -c "grep -Poh 'Verification code:\K.*,' /opt/clover/logs/*.log")
 
 echo "${key}"
 exit
