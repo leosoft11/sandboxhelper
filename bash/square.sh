3858ea94319f2d8381329b7bc85c53028ba24a0e
Anton Kuznetsov <lukarek@Antons-MacBook-Pro.local>
2021-11-12 14:40:33 +0400

    Исправил скрипт на proxy

--- bash/square.sh
+++ bash/square.sh
@@ -16,10 +16,10 @@ square_webhookV2key=${7}
 
 
 
-APPMARKET=$(kubectl get pod -n "sandbox-$SANDBOX" -l app=appmarket,hive=eu-fra1 -o jsonpath='{.items[].metadata.name}')
+proxy=$(kubectl get pod -n "sandbox-$SANDBOX" -l app=proxy,hive=eu-fra1 -o jsonpath='{.items[].metadata.name}')
 
 
-echo "update square_app set app_id = '${square_appId}', app_secret = '${square_secret}', sandbox_app_id = '${square_sandboxAppID}', sandbox_access_token = '${square_sandboxAccessToken}' , webhook_key = '${square_webhookKey}' , webhook_v2_key = '${square_webhookV2key}' where type = 'CURRENT';" | psql-sand "${SANDBOX}" appmarket
+echo "update square_app set app_id = '${square_appId}', app_secret = '${square_secret}', sandbox_app_id = '${square_sandboxAppID}', sandbox_access_token = '${square_sandboxAccessToken}' , webhook_key = '${square_webhookKey}' , webhook_v2_key = '${square_webhookV2key}' where type = 'CURRENT';" | psql-sand "${SANDBOX}" proxy
 
-kubectl -n "sandbox-$SANDBOX" exec -it $APPMARKET -c "appmarket" kill 1
+kubectl -n "sandbox-$SANDBOX" exec -it $proxy -c "proxy" kill 1
 exit
