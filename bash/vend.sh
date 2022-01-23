7d7580d3181dc25744ee8012c8115e43576e41a9
Anton Kuznetsov <lukarek@Antons-MacBook-Pro-2.local>
2022-01-24 00:42:19 +0400

    Обновил скрипт венда, убрал лишнее, тепреь меняем в нодах ClientId

--- bash/vend.sh
+++ bash/vend.sh
@@ -14,17 +14,21 @@ POD3=$(kubectl get pod -n "sandbox-$SANDBOX" -l "app=node,hive=eu-fra3" -o jsonp
 VEND=$(kubectl get pod -n "sandbox-$SANDBOX" -l app=vend,hive=eu-fra1 -o jsonpath='{.items[].metadata.name}')
 
 
-kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.serviceUrl = '${SANDBOX}'-vend.ecwid.qa" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.redisEndpoints = direct(redis-wsnext-1.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-1 redis-wsnext-2.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-2)" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.httpEndpoints = direct(https://'${SANDBOX}'-vend.ecwid.qa/wsnext)" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.secretKeys = 123456" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.clientId = '${Vend_clientID}'" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.serviceUrl = '${SANDBOX}'-vend.ecwid.qa" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.redisEndpoints = direct(redis-wsnext-1.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-1 redis-wsnext-2.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-2)" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.httpEndpoints = direct(https://'${SANDBOX}'-vend.ecwid.qa/wsnext)" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.secretKeys = 123456" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c 'echo "vend.clientId = '${Vend_clientID}'" >> /opt/service-conf/node.properties'
 
-kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.serviceUrl = '${SANDBOX}'-vend.ecwid.qa" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.redisEndpoints = direct(redis-wsnext-1.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-1 redis-wsnext-2.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-2)" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.httpEndpoints = direct(https://'${SANDBOX}'-vend.ecwid.qa/wsnext)" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.secretKeys = 123456" >> /opt/service-conf/node.properties'
-kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.clientId = '${Vend_clientID}'" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.serviceUrl = '${SANDBOX}'-vend.ecwid.qa" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.redisEndpoints = direct(redis-wsnext-1.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-1 redis-wsnext-2.sandbox-'${SANDBOX}'.svc.cluster.local:6379:sandbox-'${SANDBOX}'-redis-wsnext-2)" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.httpEndpoints = direct(https://'${SANDBOX}'-vend.ecwid.qa/wsnext)" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.secretKeys = 123456" >> /opt/service-conf/node.properties'
+# kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c 'echo "vend.clientId = '${Vend_clientID}'" >> /opt/service-conf/node.properties'
+
+kubectl -n "sandbox-$SANDBOX" exec -it $POD2 -c "node" -- bash -c "sed -i  's/\(^vend.clientId = \).*/\1${Vend_clientID}/' /opt/service-conf/node.properties"
+kubectl -n "sandbox-$SANDBOX" exec -it $POD3 -c "node" -- bash -c "sed -i  's/\(^vend.clientId = \).*/\1${Vend_clientID}/' /opt/service-conf/node.properties"
+
 
 
 kubectl -n "sandbox-$SANDBOX" exec -it $VEND -c "vend" -- bash -c "sed -i  's/\(^vend.clientId = \).*/\1${Vend_clientID}/' /opt/service-conf/vend.properties"
